1232149b557bc06894290d74d7ab8fb94b438469
nej1044 <nej1044@naver.com>
2023-03-06 18:34:51 +0900

    day01

---
+++ 1-1-c-programing.c
@@ -0,0 +1,10 @@
+/* 예제 : Hello 출력하기
+    파일 이름 : Hello.c
+    수정 일자 : 2023년 3월 6일
+    작성자: 노은정
+*/
+
+#include <stdio.h> // 헤더 파일 추가
+void main() { // main 함수 시작
+    printf("Hello~ \n"); // 모니터에 문자열 출력
+} // main 함수 끝
